e4c1160eae9e0d3f1043a784fe16648300789b7f
Nozim Mansurov <nozim.mansurov@coreteam.uz>
2024-02-27 17:40:46 +0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import { Helmet } from 'react-helmet-async'
 import HelmetProvider from '@core/contexts/HelmetContext'
-import { Suspense } from 'react'
 
 function App(props: any) {
 
 	return (
 		<>
-			<Helmet>
-				<title>ViteSSR + React + Helmet</title>
-			</Helmet>
-
 			<HelmetProvider>
 				{props.children}
 			</HelmetProvider>
